90cc10e5df17b3eb8df84c0296b90b6a4f78be90
Student <student@c009.local>
2018-12-11 18:48:46 +0300

    Updated

--- Project Light (M2A1E1)/ViewController.swift
+++ Project Light (M2A1E1)/ViewController.swift
@@ -1,25 +1,47 @@
-//
-//  ViewController.swift
-//  Project Light (M2A1E1)
-//
-//  Created by student on 28.09.2018.
-//  Copyright © 2018 STB. All rights reserved.
-//
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func buttonPressed() {
+        isOn = !isOn
+    }
+    
+    var isOn = true {
+        didSet {
+            updateUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateUI() {
+        let device =
+            AVCaptureDevice.default(for: .video)
+        if let device = device, device.hasTorch {
+            view.backgroundColor = UIColor.black
+            do {
+                try device.lockForConfiguration()
+                device.torchMode = isOn ? .on : .off
+                device.unlockForConfiguration()
+            } catch {
+                print(#function, error)
+            }
+        } else {
+        
+//        if isOn {
+//            view.backgroundColor = UIColor.white
+//        } else {
+//            view.backgroundColor = UIColor.black
+//        }
+        view.backgroundColor = isOn ? UIColor.white : UIColor.black
     }
-
-
+    
+        var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
 
+}
